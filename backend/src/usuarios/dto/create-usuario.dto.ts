a82a65ea1b4ccb4a11927198b01ab2944f01eb19
GiuDelgo <delgobbogiuliana@gmail.com>
2026-05-30 11:39:09 -0300

    fix: position fixed pantalla registro feat: configuro  modulo usuario

--- backend/src/usuarios/dto/create-usuario.dto.ts
+++ backend/src/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUsuarioDto {}
+import { IsDateString, IsString } from "class-validator";
+
+export class CreateUsuarioDto {
+    @IsString()
+    nombre!: string;
+
+    @IsString()
+    apellido!: string;
+
+    @IsString()
+    correo!: string;
+
+    @IsString()
+    usuario!: string;
+
+    @IsString()
+    contraseña!: string; 
+    
+    @IsDateString()
+    fechaNacimiento!: Date; 
+    
+    @IsString()
+    descripcion!: string; 
+    
+    @IsString()
+    foto!: string;
+    
+    @IsString()
+    perfil!: string;
+}
--- backend/src/usuarios/entities/usuario.entity.ts
+++ backend/src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,33 @@
-export class Usuario {}
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+
+@Schema()
+export class Usuario {
+    @Prop({required:true})
+    nombre!: string;
+
+    @Prop({required:true})
+    apellido!: string;
+
+    @Prop({required:true})
+    correo!: string;
+
+    @Prop({required:true})
+    usuario!: string;
+
+    @Prop({required:true})
+    contraseña!: string;
+
+    @Prop({required:true})
+    fechaNacimiento!: Date;
+
+    @Prop({required:true})
+    descripcion!: string;
+
+    @Prop({required:true})
+    foto!: string;
+
+    @Prop({required:true, default:'usuario'})
+    perfil!: string;
+}
+
+export const UsuarioSchema = SchemaFactory.createForClass(Usuario);//siempre voy a usar el schema para manejar la colección de usuarios, nunca el usuario en sí
